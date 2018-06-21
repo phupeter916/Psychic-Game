7c761c0ea5cd4a3a26c9c0a86bd796b44894992b
email <phupeter916@gmail.com>
2018-06-21 10:51:55 -0700

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,65 @@
-alert("What is your guess?");
+
+
+
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessleft = 10;
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
+
+    
+
+    
+
+    if (userGuess === computerGuess) {
+        wins++; guessleft--;
+    }
+
+    if (userGuess != computerGuess) {
+        losses++; guessleft--;
+    }
+
+    var html =
+    "<p>wins: " + wins + "</p>" +
+    "<p>losses: " + losses + "</p>" +
+    "<p>guessleft: " + guessleft + "</p>" +
+    <p>guess:  + guess + </p>;
+
+    
+    
+
+
+
+    
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
